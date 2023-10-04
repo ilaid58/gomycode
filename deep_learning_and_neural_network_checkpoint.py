420e64b50545d91a01bce71a543ef66f60d93159
ilaid58 <96706139+ilaid58@users.noreply.github.com>
2023-10-04 18:35:57 +0000

    Update deep_learning_and_neural_network_checkpoint.py

--- deep_learning_and_neural_network_checkpoint.py
+++ deep_learning_and_neural_network_checkpoint.py
@@ -13,7 +13,7 @@ Original file is located at
 import pip
 
 pip.main(['install','SpeechRecognition'])
-pip.main(['install', 'nltk', '--adminuser'])
+pip.main(['install', 'nltk'])
 import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
